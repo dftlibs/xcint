47079e9ed4ab91adb2f85ec7165abeae6fe30b17
Radovan Bast <bast@users.noreply.github.com>
2015-06-22 22:48:16 +0200

    update autocmake

--- cmake/update.py
+++ cmake/update.py
@@ -5,12 +5,14 @@ import sys
 import shutil
 from collections import OrderedDict
 
-if sys.version_info.major > 2:
+# we do not use the nicer sys.version_info.major
+# for compatibility with Python < 2.7
+if sys.version_info[0] > 2:
     from configparser import RawConfigParser
 else:
     from ConfigParser import RawConfigParser
 
-if sys.version_info.major > 2:
+if sys.version_info[0] > 2:
     import urllib.request
     class URLopener(urllib.request.FancyURLopener):
         def http_error_default(self, url, fp, errcode, errmsg, headers):
@@ -90,6 +92,8 @@ def gen_cmake_command(config):
             for definition in config.get(section, 'define').split('\n'):
                 s.append('    command.append(%s)' % definition)
 
+    s.append("    command.append('-DCMAKE_BUILD_TYPE=%s' % arguments['--type'])")
+
     s.append("\n    return ' '.join(command)")
 
     return '\n'.join(s)
@@ -125,6 +129,7 @@ def gen_setup(config, relative_path):
                 rest = ' '.join(opt.split()[1:]).strip()
                 options.append([first, rest])
 
+    options.append(['--type=<TYPE>', 'Set the CMake build type (debug, release, or relwithdeb) [default: release].'])
     options.append(['--show', 'Show CMake command and exit.'])
     options.append(['<builddir>', 'Build directory.'])
     options.append(['-h --help', 'Show this screen.'])

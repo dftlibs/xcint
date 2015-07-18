9b0bef49a368af539314402b2fd1a9da09574625
Radovan Bast <bast@users.noreply.github.com>
2015-07-18 11:57:27 +0200

    phase out ConfigExternal.cmake

--- cmake/custom/custom.cmake
+++ cmake/custom/custom.cmake
@@ -1,3 +1,6 @@
+include(ExternalProject)
+
+
 # FIXME
 option(ENABLE_FORTRAN_INTERFACE "Build Fortran interface" OFF)
 if(ENABLE_FORTRAN_INTERFACE)
@@ -15,17 +18,33 @@ set(ExternalProjectCMakeArgs
     -DENABLE_FORTRAN_INTERFACE=OFF
     -DENABLE_STATIC_LINKING=ON # we need the -fPIC
     )
-add_external(xcfun)
+ExternalProject_Add(xcfun
+    SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/xcfun
+    BINARY_DIR ${PROJECT_BINARY_DIR}/external/xcfun-build
+    STAMP_DIR ${PROJECT_BINARY_DIR}/external/xcfun-stamp
+    TMP_DIR ${PROJECT_BINARY_DIR}/external/xcfun-tmp
+    INSTALL_DIR ${PROJECT_BINARY_DIR}/external
+    CMAKE_ARGS ${ExternalProjectCMakeArgs}
+    )
+
 
 # numgrid code
-set(ExternalProjectCMakeArgs
-    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-    -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/external
+set(numgrid_args
     -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
     -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+    -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/external
     )
-add_external(numgrid)
+ExternalProject_Add(numgrid
+    SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/numgrid
+    BINARY_DIR ${PROJECT_BINARY_DIR}/external/numgrid-build
+    STAMP_DIR ${PROJECT_BINARY_DIR}/external/numgrid-stamp
+    TMP_DIR ${PROJECT_BINARY_DIR}/external/numgrid-tmp
+    INSTALL_DIR ${PROJECT_BINARY_DIR}/external
+    CMAKE_ARGS ${numgrid_args}
+    )
 include_directories(${PROJECT_SOURCE_DIR}/external/numgrid/api)
+
 
 include_directories(
     ${PROJECT_SOURCE_DIR}/src

52450171cc6922a6fc76d43bb7631dbf1e5c8828
Radovan Bast <bast@users.noreply.github.com>
2017-01-31 20:16:49 +0100

    use master tip of balboa

--- cmake/custom/balboa.cmake
+++ cmake/custom/balboa.cmake
@@ -4,7 +4,7 @@ ExternalProject_Add(
     balboa
     PREFIX "${PROJECT_BINARY_DIR}/balboa"
     GIT_REPOSITORY https://github.com/bast/balboa.git
-    GIT_TAG a26be6df03c3736c96e401b1a3b0c2d9f4f8272c
+    GIT_TAG master
     INSTALL_COMMAND true  # currently no install command
     )
 

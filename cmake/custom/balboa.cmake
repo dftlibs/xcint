f4a6ebd76159bafd14c0c87a054b33dbc5bc3808
Radovan Bast <bast@users.noreply.github.com>
2017-11-12 18:27:29 +0100

    forward cmake build type to balboa lib

--- cmake/custom/balboa.cmake
+++ cmake/custom/balboa.cmake
@@ -6,6 +6,7 @@ ExternalProject_Add(
     GIT_REPOSITORY https://github.com/bast/balboa.git
     GIT_TAG a4cbe0cf7d7f9e0c6e0c22644ea8a8010a33701a
     INSTALL_COMMAND true  # currently no install command
+    CMAKE_ARGS "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
     )
 
 link_directories(${PROJECT_BINARY_DIR}/balboa/src/balboa-build/lib)

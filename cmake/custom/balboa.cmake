d2e22f7c8567d873c0d84ea806fff99f69372ae9
Radovan Bast <bast@users.noreply.github.com>
2018-01-13 18:51:23 +0100

    update balboa dependency

--- cmake/custom/balboa.cmake
+++ cmake/custom/balboa.cmake
@@ -4,7 +4,7 @@ ExternalProject_Add(
     balboa
     PREFIX "${PROJECT_BINARY_DIR}/balboa"
     GIT_REPOSITORY https://github.com/bast/balboa.git
-    GIT_TAG a4cbe0cf7d7f9e0c6e0c22644ea8a8010a33701a
+    GIT_TAG b7fc688e890ad728e07d505802eedbaf54a1ad9e
     INSTALL_COMMAND true  # currently no install command
     CMAKE_ARGS "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
     )
@@ -23,5 +23,5 @@ set(BALBOA_LIBS
     libbalboa.${_dyn_lib_suffix}
     )
 
-include_directories(${PROJECT_BINARY_DIR}/balboa/src/balboa/api)
+include_directories(${PROJECT_BINARY_DIR}/balboa/src/balboa/balboa)
 include_directories(${PROJECT_BINARY_DIR}/balboa/src/balboa-build/include)

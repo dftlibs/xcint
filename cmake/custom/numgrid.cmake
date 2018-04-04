f6c208507d04d51f3cd0800b889b90b4b381647e
Radovan Bast <bast@users.noreply.github.com>
2018-04-04 18:04:01 +0200

    pass fortran compiler on to numgrid

--- cmake/custom/numgrid.cmake
+++ cmake/custom/numgrid.cmake
@@ -7,8 +7,9 @@ ExternalProject_Add(
     GIT_TAG 6eabd3e1806383089fedba986780d21739975caf
     INSTALL_COMMAND true  # currently no install command
     CMAKE_ARGS
-        "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
-#       "-DENABLE_UNIT_TESTS=OFF"
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER}
+        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
     )
 
 link_directories(${PROJECT_BINARY_DIR}/numgrid/src/numgrid-build/src)

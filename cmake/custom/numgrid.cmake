679fcdeb7a38ac51b9ce2041a5d4f0ae83658a91
Radovan Bast <bast@users.noreply.github.com>
2018-10-23 17:32:59 +0200

    upgrade numgrid

--- cmake/custom/numgrid.cmake
+++ cmake/custom/numgrid.cmake
@@ -1,19 +1,20 @@
 include(ExternalProject)
 
 ExternalProject_Add(
-    numgrid
-    PREFIX "${PROJECT_BINARY_DIR}/numgrid"
-    GIT_REPOSITORY https://github.com/dftlibs/numgrid.git
-    GIT_TAG 6eabd3e1806383089fedba986780d21739975caf
-    INSTALL_COMMAND true  # currently no install command
-    CMAKE_ARGS
-        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER}
-        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-    )
+  numgrid
+  PREFIX "${PROJECT_BINARY_DIR}/numgrid"
+  GIT_REPOSITORY https://github.com/dftlibs/numgrid.git
+  GIT_TAG e4d16a3d8dc02541520c5975334567344d394916
+  INSTALL_COMMAND true  # currently no install command
+  CMAKE_ARGS
+    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+    -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER}
+    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+    -DENABLE_UNIT_TESTS=OFF
+  )
 
-link_directories(${PROJECT_BINARY_DIR}/numgrid/src/numgrid-build/src)
-link_directories(${PROJECT_BINARY_DIR}/numgrid/src/numgrid-build/lib)
+include(GNUInstallDirs)
+link_directories(${PROJECT_BINARY_DIR}/numgrid/src/numgrid-build/${CMAKE_INSTALL_LIBDIR})
 
 # workaround:
 # different dynamic lib suffix on mac
@@ -29,4 +30,5 @@ set(NUMGRID_LIBS
 
 include_directories(${PROJECT_BINARY_DIR}/numgrid/src/numgrid/numgrid)
 include_directories(${PROJECT_BINARY_DIR}/numgrid/src/numgrid-build/modules)
+include_directories(${PROJECT_BINARY_DIR}/numgrid/src/numgrid-build/numgrid)
 include_directories(${PROJECT_BINARY_DIR}/numgrid/src/numgrid-build/include)

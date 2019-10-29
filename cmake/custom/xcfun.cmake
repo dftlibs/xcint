8b65738b10019ac7bcb64ca9096876708dc07baa
Radovan Bast <bast@users.noreply.github.com>
2019-10-29 14:22:19 +0100

    use FetchContent for XCFun
    instead of previously submodule and cmake mess

--- cmake/custom/xcfun.cmake
+++ cmake/custom/xcfun.cmake
@@ -1,21 +1,24 @@
-include(ExternalProject)
+include(FetchContent)
 
-set(ExternalProjectCMakeArgs
+FetchContent_Populate(xcfun_sources
+  QUIET
+  GIT_REPOSITORY
+    https://github.com/dftlibs/xcfun.git
+  GIT_TAG
+    ca137ba62577796b6cf916fbb83ab79784096ecf
+  CMAKE_ARGS
     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-    -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/external
     -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
     -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
     -DENABLE_FORTRAN_INTERFACE=OFF
     -DENABLE_TESTALL=OFF
-    -DENABLE_STATIC_LINKING=ON # we need the -fPIC
     -DXCFun_XC_MAX_ORDER=6
-    )
+  CMAKE_CACHE_ARGS
+    -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
+    -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
+  )
 
-ExternalProject_Add(xcfun
-    SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/xcfun
-    BINARY_DIR ${PROJECT_BINARY_DIR}/external/xcfun-build
-    STAMP_DIR ${PROJECT_BINARY_DIR}/external/xcfun-stamp
-    TMP_DIR ${PROJECT_BINARY_DIR}/external/xcfun-tmp
-    INSTALL_DIR ${PROJECT_BINARY_DIR}/external
-    CMAKE_ARGS ${ExternalProjectCMakeArgs}
-    )
+add_subdirectory(
+  ${xcfun_sources_SOURCE_DIR}
+  ${xcfun_sources_BINARY_DIR}
+  )
